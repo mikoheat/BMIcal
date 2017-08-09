8268566ed160ae0578d2562a2af39f947a9c5028
Taeho <taehoworld3126@gmail.com>
2017-08-08 20:11:09 -0400

    Created SplashForm

---
+++ BMICalculator/SplashForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* 
+ * Name: Taeho Kim
+ * Date: Aug 8, 2017
+ * Description: This is the SplashForm class
+ * Version: 0.1 - Created the SplashForm
+ */
+
+namespace BMICalculator
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
